f65539ab078cc041bcde319a6ce38cad156afb09
asadas1 <45112956+asadas1@users.noreply.github.com>
2020-07-09 21:44:25 -0400

    ss

--- laptop/runner.py
+++ laptop/runner.py
@@ -16,54 +16,11 @@ from google.auth.transport.requests import Request
 import xlrd 
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.events', 'https://www.googleapis.com/auth/spreadsheets']
+SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/spreadsheets']
 
 #Root window for TK
 root = tk.Tk()
 root.withdraw()
-
-# Give the location of the file 
-loc = askopenfilename(title = "Select EXCEL file",filetypes = (("xlsx files","*.xlsx"), ("all files","*.*")) )
-  
-#This part is about parsing the Excel file into the variables needed to store the event info
-# To open Workbook 
-wb = xlrd.open_workbook(loc) 
-sheet = wb.sheet_by_index(0) 
-
-#Get Title/Summary
-summary_in = (sheet.cell_value(1,0))
-
-#Get Location
-loc_in = (sheet.cell_value(1,1))
-
-#Get Desc
-desc_in = (sheet.cell_value(1,2))
-
-#Get Start Time and Date
-starttime_in = (sheet.cell_value(1,3))
-startdate_in = (sheet.cell_value(1,4))
-start_dts = startdate_in + ' ' + starttime_in
-
-#Get End Time and Date
-endtime_in = (sheet.cell_value(1,5))
-enddate_in = (sheet.cell_value(1,6))
-end_dts = enddate_in + ' ' + endtime_in
-
-#Date & timestamp stuff is janky because the JSON object "event" wants RCF formatted time,
-#whereas the Excel file could have any kind of time input, so using strptime with concacted strings is probably the most
-#flexible approach for now
-dto_start = datetime.datetime.strptime(start_dts, '%m-%d-%Y %I:%M %p')
-dto_end = datetime.datetime.strptime(end_dts, '%m-%d-%Y %I:%M %p')
-
-#Get Attendees // currently not implemented
-#List of attendees is a "list of dicts" which is the input the JSON object "event" wants
-#attendee = (sheet.cell_value(7,1))
-attendees = ["lpage@example.com", "ddage@example.com"]
-list_of_attendees = [
-    {'email': attendees[0] },
-    {'email': attendees[1] }
-    ]
-#Is a WIP
 
 def main():
     # A quick check to see if the token already exists.
@@ -111,44 +68,109 @@ def main():
     values = result.get('values', [])
     print (len(values))
 
+    search = []
+
     if not values:
         print('No data found.')
     else:
         for row in values:
-            print (len(row))
+            #print (len(row))
             # Print columns A and E, which correspond to indices 0 and 4.
-            print(row[0] + ' ' + row[1] + ' ' + row[4] + ' ' + row[5] + ' ' + row[6] + ' ' + row[7] + ' ' + row[8] + ' ' + row[9] + ' ' + row[10] + ' ' + row[11] + ' ' + row[12] )
+            if (row[0] == '07/01/2020'):
+                search.append(row)
+                print('0 ' + row[0] + ' 1 ' + row[1] + ' 4 ' + row[4] + ' 5 ' + row[5] + ' 6 ' + row[6] + ' 7 ' + row[7] + ' 8 ' + row[8] + ' 9 ' + row[9] + ' 10 ' + row[10] + ' 11 ' + row[11] + ' 12 ' + row[12] + ' 13 ' + row[13] )
 
-    #The actual JSON style event object, time zone is static just because not really necessary 
-    event = {
-      'summary': summary_in,
-      'location': loc_in,
-      'description': desc_in,
-      'start': {
-        'dateTime': dto_start.isoformat("T"),
-        'timeZone': 'US/Eastern',
-      },
-      'end': {
-        'dateTime': dto_end.isoformat("T"),
-        'timeZone': 'US/Eastern',
-      },
-     # 'recurrence': [
-     #   'RRULE:FREQ=DAILY;COUNT=2'
-     # ],
-      'attendees': list_of_attendees,
-      'reminders': {
-        'useDefault': False,
-        'overrides': [
-          {'method': 'email', 'minutes': 24 * 60},
-          {'method': 'popup', 'minutes': 10},
-        ],
-      },
-    }
-    
-    #Uses the service to insert the event
-    event = service.events().insert(calendarId='primary', body=event, sendUpdates='all').execute()
-    #could possibly make a popup with the HTML link as output
-    print ('Event created: %s' % (event.get('htmlLink')))
+    for row in search:
+        #Get Title/Summary
+        summary_in = (row[10])
+
+        #Get Location
+        loc_in = (row[1])
+
+        #Get Desc
+        desc_in = (row[10])
+
+        #Get Start Time and Date
+        start_dts = row[0] + ' ' + row[5]
+
+        #Get End Time and Date
+        end_dts = row[0] + ' ' + row[6]
+
+        #Date & timestamp stuff is janky because the JSON object "event" wants RCF formatted time,
+        #whereas the Excel file could have any kind of time input, so using strptime with concacted strings is probably the most
+        #flexible approach for now
+        dto_start = datetime.datetime.strptime(start_dts, '%m/%d/%Y %I:%M %p')
+        dto_end = datetime.datetime.strptime(end_dts, '%m/%d/%Y %I:%M %p')
+
+        #Get Attendees // currently not implemented
+        #List of attendees is a "list of dicts" which is the input the JSON object "event" wants
+        #attendees = ["lpage@example.com", "ddage@example.com"]
+        attendee = row[9].split(',')
+        sttendee = attendee[0]
+
+        print(sttendee)
+        list_of_attendees = [
+            {'email': sttendee+'@example.com'}
+            ]
+        if row[11]:
+            print (row[11])
+            a0dee = row[11].split(',')
+            a1dee = a0dee[0].split()
+            if a1dee:
+                a2dee = a1dee[0]
+                print(a2dee)
+                list_of_attendees.append({'email': a2dee+'@example.com'})
+        if row[12]:
+            print (row[12])
+            a0dee = row[12].split(',')
+            print(a0dee)
+            a1dee = a0dee[0].split()
+            print(a1dee)
+            if a1dee:
+                a2dee = a1dee[0]
+                print(a2dee)
+                list_of_attendees.append({'email': a2dee+'@example.com'})
+        if row[13]:
+            print (row[13])
+            a0dee = row[11].split(',')
+            a1dee = a0dee[0].split()
+            if a1dee:
+                a2dee = a1dee[0]
+                print(a2dee)
+                list_of_attendees.append({'email': a2dee+'@example.com'})
+        #Is a WIP
+
+
+        #The actual JSON style event object, time zone is static just because not really necessary 
+        event = {
+          'summary': summary_in,
+          'location': loc_in,
+          'description': desc_in,
+          'start': {
+            'dateTime': dto_start.isoformat("T"),
+            'timeZone': 'US/Eastern',
+          },
+          'end': {
+            'dateTime': dto_end.isoformat("T"),
+            'timeZone': 'US/Eastern',
+          },
+         # 'recurrence': [
+         #   'RRULE:FREQ=DAILY;COUNT=2'
+         # ],
+          'attendees': list_of_attendees,
+          'reminders': {
+            'useDefault': False,
+            'overrides': [
+              {'method': 'email', 'minutes': 24 * 60},
+              {'method': 'popup', 'minutes': 10},
+            ],
+          },
+        }
+        
+        #Uses the service to insert the event
+        event = service.events().insert(calendarId='primary', body=event, sendUpdates='all').execute()
+        #could possibly make a popup with the HTML link as output
+        print ('Event created: %s' % (event.get('htmlLink')))
 
 if __name__ == '__main__':
     main()
